5434cff71e883a966c857c2c2c5e98950b3ff8a2
Suheyl Turac <skbsturac@live.com>
2018-09-25 00:52:46 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,13 +1,13 @@
-def roll_call_dwarves(array) # code an argument here
+def roll_call_dwarves(array)                     # code an argument here
   # Your code here
-  
-  #pack = array.new
+  pack = []
+  #pack = Array.new
   array.each_with_index do |name, index|
-  #pack << ("#{index+1}. #{name}")
-  array << ("#{index+1}. #{name}")
+  pack << "#{index+1}: #{name}"
+  #array << ("#{index+1}. #{name}")
 end
- #puts pack
- puts "(#{index+1}. #{name})"
+ puts pack
+ #puts "#{index+1}. #{name}"
 end
 
 def summon_captain_planet(long_planeteer_calls)  # code an argument here
